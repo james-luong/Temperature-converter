ccb45537035078dd50ca6bfa5b2291d98349f089
james-luong <83560549+james-luong@users.noreply.github.com>
2022-03-29 12:37:04 +1300

    Rounding numbers

---
+++ Components/Component 3 - C to F/03_rounding_trial_1.py
@@ -0,0 +1,16 @@
+"""Display output using rounding for floats only
+"""
+
+to_round = [1/1, 1/2, 1/3]
+
+print('Number to round:')
+print(to_round)
+
+print('Rounded number:')
+
+for item in to_round:
+    if item.is_integer():
+        print('{:.0f}'.format(item))
+    else:
+        print('{:.1f}'.format(int(item)))
+
