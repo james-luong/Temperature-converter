c840f7a1ec44e4b8c143b97df79eafb8762a700a
james-luong <83560549+james-luong@users.noreply.github.com>
2022-03-28 16:31:09 +1300

    Update 02_Converter_GUI_v1.py

--- Components/Component 2 - Converter GUI/02_Converter_GUI_v1.py
+++ Components/Component 2 - Converter GUI/02_Converter_GUI_v1.py
@@ -35,6 +35,47 @@ class Converter:
                                             padx=10, pady=10)
         self.temp_instruction_label.grid(row=1)
 
+        # temperature entry box
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font='arial 14 bold')
+        self.to_convert_entry.grid(row=2)
+
+        # conversion buttons frame (row 3), orchid3 | khaki1
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_conversion = Button(self.conversion_buttons_frame,
+                                      text='Centigrade', font='arial 10 bold',
+                                      bg='khaki1', padx=10, pady=10)
+        self.to_c_conversion.grid(row=0, column=0)
+
+        self.to_f_conversion = Button(self.conversion_buttons_frame,
+                                      text='Fahrenheit', font='arial 10 bold',
+                                      bg='orchid1', padx=10, pady=10)
+        self.to_f_conversion.grid(row=0, column=1)
+
+
+        # answer label (row 4)
+        self.converted_label = Label(self.converter_frame,
+                                     font='Arial 12 bold',
+                                     fg='purple', bg=background_colour,
+                                     pady=10, text='Conversion goes here...')
+        self.converted_label.grid(row=4)
+
+        # history/help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame,
+                                       font='Arial 12 bold',
+                                       text='Calculation History',
+                                       width=15)
+        self.calc_hist_button.grid(row=0, column=0)
+
+        self.help_button = Button(self.hist_help_frame, font='Arial 12 bold',
+                                  text='Help', width=5)
+        self.help_button.grid(row=0, column=1)
+
 
 # main routine
 if __name__ == "__main__":
